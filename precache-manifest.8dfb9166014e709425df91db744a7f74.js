77c2fb8cc81a39c6d1343d61975b37899db4796b
ceyhuncengiz <68386773+ceyhuncengiz@users.noreply.github.com>
2020-10-25 22:04:10 +0300

    Updates

---
+++ precache-manifest.8dfb9166014e709425df91db744a7f74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7bbf24a62bc536b638d1a5933271cd4",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "38ac49b7ef527f258978",
+    "url": "/monsters-rolodex/static/css/main.b929ddc3.chunk.css"
+  },
+  {
+    "revision": "88b685495fee72b0273c",
+    "url": "/monsters-rolodex/static/js/2.2eaf0afe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monsters-rolodex/static/js/2.2eaf0afe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38ac49b7ef527f258978",
+    "url": "/monsters-rolodex/static/js/main.c46b36c1.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
